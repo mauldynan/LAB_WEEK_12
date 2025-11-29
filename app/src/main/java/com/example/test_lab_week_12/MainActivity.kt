b66fda061578b306b87fda3dcd4157e2ca91fde2
Maulidya Isnaini <163633669+mauldynan@users.noreply.github.com>
2025-11-29 14:25:50 +0700

    Commit No. 2 – Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
+++ app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.test_lab_week_12
 
+import androidx.lifecycle.Lifecycle
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test_lab_week_12.model.Movie
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
     private val movieAdapter by lazy {
         MovieAdapter(object : MovieAdapter.MovieClickListener {
             override fun onMovieClick(movie: Movie) {
@@ -19,8 +25,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val recyclerView: RecyclerView = findViewById(R.id.movie_list)
         recyclerView.adapter = movieAdapter
+
+        val movieRepository = (application as MovieApplication).movieRepository
+
+        val movieViewModel = ViewModelProvider(
+            this,
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return MovieViewModel(movieRepository) as T
+                }
+            }
+        )[MovieViewModel::class.java]
+
+        movieViewModel.popularMovies.observe(this) { movies ->
+            movieAdapter.addMovies(movies)
+        }
+
+        movieViewModel.error.observe(this) { error ->
+            if (error.isNotEmpty()) {
+                Snackbar.make(recyclerView, error, Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 
     private fun openMovieDetails(movie: Movie) {
--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -1,7 +1,33 @@
 package com.example.test_lab_week_12
 
 import com.example.test_lab_week_12.api.MovieService
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MovieRepository(private val movieService: MovieService) {
-
+    private val apiKey = "0e9b4208d688b2db79f06a76dc24bdae"
+    // LiveData that contains a list of movies
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+    // LiveData that contains an error message
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = errorLiveData
+    // fetch movies from the API
+    suspend fun fetchMovies() {
+        try {
+// get the list of popular movies from the API
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(popularMovies.results)
+        } catch (exception: Exception) {
+// if an error occurs, post the error message to the
+            errorLiveData
+            errorLiveData.postValue(
+                "An error occurred: ${exception.message}")
+        }
+    }
 }
